54902feda606a03d248d452229b2d731269a333e
Thomas Buchinger <thomas.buchinger@outlook.com>
2026-05-31 18:25:32 +0200

    fix: naming conflict

--- prompts/coding/swe.py
+++ prompts/coding/swe.py
@@ -15,17 +15,17 @@ def grill_me(prompt = "") -> str:
 def grill_me_adr() -> str:
     return (SKILLS_DIR / "coding" / "mattpocock_grill-me" / "resources" / "ADR-FORMAT.md").read_text()
 @resource("data://mattpocock_grill-me/resources/CONTEXT-FORMAT.md")
-def grill_me_adr() -> str:
+def grill_me_context() -> str:
     return (SKILLS_DIR / "coding" / "mattpocock_grill-me" / "resources" / "CONTEXT-FORMAT.md").read_text()
 
 
 @prompt(description=skill_description(SKILLS_DIR / "coding" / "mattpocock_tdd"))
-def grill_me(prompt = "") -> str:
+def tdd(prompt = "") -> str:
     content = skill_content(SKILLS_DIR / "coding" / "mattpocock_tdd")
     return content + prompt
 
 @prompt(description=skill_description(SKILLS_DIR / "coding" / "mattpocock_to-prd"))
-def grill_me(prompt = "") -> str:
+def frill_me_to_prd(prompt = "") -> str:
     content = skill_content(SKILLS_DIR / "coding" / "mattpocock_to-prd")
     return content + prompt
 
